d0d2848951b2dae0e70221bd94ea9f01d7885d8b
JAGORING <lovehaha11@gmail.com>
2024-12-02 04:27:45 +0900

    [feat] middleware 적용

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getSession } from './lib/session';
+import { redirect } from 'next/navigation';
+
+interface Routes {
+  [key: string]: boolean;
+}
+const publicOnlyUrls: Routes = {
+  '/': true,
+  '/create-account': true,
+};
+
+export const middleware = async (request: NextRequest) => {
+  const session = await getSession();
+  const { pathname } = request.nextUrl;
+  const exists = publicOnlyUrls[pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL('/user-profile', request.url));
+    }
+  }
+};
+
+export const config = {
+  matcher: ['/', '/profile', '/create-account'],
+};
