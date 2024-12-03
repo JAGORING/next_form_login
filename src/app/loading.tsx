e182f008cecad7fc0c5bf706e3369338ff61fa25
JAGORING <lovehaha11@gmail.com>
2024-12-04 03:25:18 +0900

    [feat] tweet Detail Db 데이터 연결 및 UI 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#fdfcf9]">
-      <div className="text-center">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#6b4f4f] border-opacity-80 mb-4"></div>
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="flex flex-col items-center justify-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#6b4f4f] border-opacity-80 mb-8"></div>
         <p className="text-[#6b4f4f] text-lg font-semibold">Loading, please wait...</p>
       </div>
     </div>
---
+++ src/app/tweets/[id]/page.tsx
@@ -0,0 +1,63 @@
+import db from '@/lib/db';
+import { formatDate } from '@/utils/date';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+
+interface Tweet {
+  id: number;
+  title: string;
+  tweet: string;
+  created_at: string;
+}
+
+const getTweetDetail = async (id: number) => {
+  const tweetData = await db.tweet.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+        },
+      },
+    },
+  });
+  return tweetData;
+};
+
+const TweetDetail = async ({ params }: { params: { id: number } }) => {
+  const id = Number(params.id);
+
+  if (isNaN(id)) {
+    return notFound();
+  }
+
+  const tweetDetail = await getTweetDetail(id);
+  if (!tweetDetail) {
+    return notFound();
+  }
+
+  if (!tweetDetail) return <p>Loading tweet details...</p>;
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-md p-6 bg-[#fdfcf9] shadow-xl rounded-2xl border border-[#e2ddd7]">
+        <Link href="/" className="text-[#6b4f4f] text-sm mb-4 block hover:underline">
+          ← Back to Tweets
+        </Link>
+
+        <h2 className="text-2xl font-semibold text-center text-[#6b4f4f] mb-6">🐦 Tweet Detail</h2>
+        <div className="p-4 bg-white shadow rounded-lg border border-[#e2ddd7]">
+          <p className="font-semibold text-[#6b4f4f]">
+            {tweetDetail.title} <span className="font-medium text-[#6b4f4f] text-xs">@{tweetDetail.user.username}</span>
+          </p>
+          <p className="text-sm text-[#8a6a6a]">{formatDate(tweetDetail.created_at)}</p>
+          <p className="mt-4 text-[#4a4a4a]">{tweetDetail.tweet}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TweetDetail;
