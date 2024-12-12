8d1f4efa62b1b080c26160d37a588a537b1ddc1f
JAGORING <lovehaha11@gmail.com>
2024-12-13 01:01:06 +0900

    [feat] main 페이지에서 Tweet > 100자 넘어가면 ...로 축약

--- src/app/(home)/components/TweetBox.tsx
+++ src/app/(home)/components/TweetBox.tsx
@@ -7,12 +7,13 @@ interface TweetBoxProps {
 }
 
 const TweetBox = ({ tweet }: TweetBoxProps) => {
+  const truncatedTweet = tweet.tweet.length > 100 ? `${tweet.tweet.slice(0, 100)}...` : tweet.tweet;
   return (
     <Link href={`/tweets/${tweet.id}`}>
       <div key={tweet.id} className="mb-3 p-4 bg-white shadow rounded-lg border border-[#e2ddd7]">
         <p className="font-semibold text-[#6b4f4f]">@{tweet.title}</p>
         <p className="text-sm text-[#8a6a6a]">{formatDate(tweet.created_at)}</p>
-        <p className="mt-2 text-[#4a4a4a]">{tweet.tweet}</p>
+        <p className="mt-2 text-[#4a4a4a]">{truncatedTweet}</p>
       </div>
     </Link>
   );
